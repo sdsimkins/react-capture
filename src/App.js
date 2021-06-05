be3db79a07eb0699e1d2d75e3d971887bdd1aad5
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-06-04 22:02:48 -0400

    11. Movie Details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MoveDetail";
 // Router 
 import {Switch, Route} from "react-router-dom";
 
@@ -19,8 +20,12 @@ function App() {
           <AboutUs />
         </Route>
 
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
 
         <Route path="/contact">
